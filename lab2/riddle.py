bd80ad366750d083c734274065ec809db83d5121
Andrea Stedile <andrea.stedile@studenti.unitn.it>
2021-04-08 11:42:39 +0200

    Assert the correctness of the model

--- lab2/riddle.py
+++ lab2/riddle.py
@@ -114,6 +114,10 @@ for job in ["c", "g", "s", "m"]:
 
 output_file.write("c Add this problem line: p cnf {} {}".format(index, n_clauses))
 g = Glucose3(bootstrap_with=cnf.clauses)
-print(cnf)
 g.solve()
-print(g.get_model())
+model = g.get_model()
+print(model)
+assert model == [-1, -2, 3, -4, 5, -6, -7, -8,
+                 -9, -10, -11, 12, -13, -14, 15, -16,
+                 17, -18, -19, -20, -21, -22, -23, 24,
+                 -25, 26, -27, -28, -29, 30, -31, -32]
